d6ff44b7ec16aa304cb78f0a6c459d64e1d54cd7
qiuzhe <2810201146@qq.com>
2018-09-14 12:44:55 +0800

    9/14
    项目包（不含谷歌浏览器安装包）/ 遗留找不到excel文档问题

---
+++ Leap_Tk/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('UserApp.urls')), 
+    path('ckeditor', include('ckeditor_uploader.urls')),
+    path('Answer/', include('TkApp.urls')), 
+    path('myadmin/', include('My_Admin.urls') ,name = "myadmin"), 
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
